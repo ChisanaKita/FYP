1814cd51e9c3d579c810f86fe045ccd22f72c9e9
ChisanaKita <29787181+ChisanaKita@users.noreply.github.com>
2020-12-31 22:54:05 +0800

    Experimental Reel Handle

---
+++ Assets/Scripts/Test_Grabbing.cs
@@ -0,0 +1,25 @@
+﻿//https://www.youtube.com/watch?v=3cJ_uq1m-dg
+using UnityEngine;
+
+public class Test_Grabbing : MonoBehaviour
+{
+    public Transform Target;
+    private Rigidbody _Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _Rigidbody.MovePosition(Target.transform.position);
+
+    }
+}
