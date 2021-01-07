06d74b7fd95974a40f1676e99a98d5a484e583ec
ChisanaKita <29787181+ChisanaKita@users.noreply.github.com>
2021-01-08 07:07:04 +0800

    Full VR Fishing Rod Controls Completed
    - Added `Reel.cs`
    > Triggers reel up/down events.
    
    - Changed `Hand.cs`
    > Added object interaction function
    - Changed project version to `0.5`

---
+++ Assets/Scripts/Reel.cs
@@ -0,0 +1,51 @@
+﻿//https://stackoverflow.com/questions/55451878/counting-a-full-rotation-with-a-circular-drive
+using UnityEngine;
+using VRF.Driver;
+
+namespace VRF
+{
+    public class Reel : MonoBehaviour
+    {
+        public int RotationCount;
+
+        public float rotatedAroundX;
+
+        public Vector3 lastUp;
+
+
+        private void Awake()
+        {
+            rotatedAroundX = 0;
+
+            // initialize
+            lastUp = transform.up;
+        }
+
+        private void Update()
+        {
+            var rotationDifference = Vector3.SignedAngle(transform.up, lastUp, transform.forward);
+
+            rotatedAroundX += rotationDifference;
+
+            if (rotatedAroundX >= 360.0f)
+            {
+                Debug.Log("One positive rotation done", this);
+
+                EntityDriver.Instance.TriggerReelUp();
+
+                 rotatedAroundX -= 360.0f;
+            }
+            else if (rotatedAroundX <= -360.0f)
+            {
+                Debug.Log("One negative rotation done", this);
+
+                EntityDriver.Instance.TriggerReelDown();
+
+                rotatedAroundX += 360.0f;
+            }
+
+            // update last rotation
+            lastUp = transform.up;
+        }
+    }
+}
--- Assets/Scripts/VR/Hand.cs
+++ Assets/Scripts/VR/Hand.cs
@@ -12,15 +12,17 @@ namespace VRF
 
         public SteamVR_Input_Sources CurrentHandInput = SteamVR_Input_Sources.LeftHand;
 
-        private bool IsHandFree;
+        private bool IsHandGrabbing;
+        private bool IsHandInteracting;
         private Rigidbody _HoldingObject;
+        private Rigidbody _InteractingObject;
         public LayerMask _GrabLayer;
-        private LayerMask _InteractLayer;
+        public LayerMask _InteractLayer;
 
         // Start is called before the first frame update
         void Start()
         {
-            IsHandFree = true;
+            IsHandGrabbing = true;
             _GrabLayer = LayerMask.GetMask("Grabbable");
             _InteractLayer = LayerMask.GetMask("Interactable");
         }
@@ -29,14 +31,15 @@ namespace VRF
         void FixedUpdate()
         {
             #region Pick_Up_Object
-            IsHandFree = SteamVR_Actions.VRF.Grab.GetState(CurrentHandInput);
+            IsHandGrabbing = SteamVR_Actions.VRF.Grab.GetState(CurrentHandInput);
+            IsHandInteracting = SteamVR_Actions.VRF.PullTrigger.GetState(CurrentHandInput);
 
-            if (!IsHandFree)
+            if (!IsHandGrabbing)
             {
-                Collider[] colliders = Physics.OverlapSphere(transform.position, PICKUP_DISTANCE, _GrabLayer);
-                if (colliders.Length > 0)
+                Collider[] Grabbables = Physics.OverlapSphere(transform.position, PICKUP_DISTANCE, _GrabLayer);
+                if (Grabbables.Length > 0)
                 {
-                    _HoldingObject = colliders[0].transform.root.GetComponent<Rigidbody>();
+                    _HoldingObject = Grabbables[0].transform.root.GetComponent<Rigidbody>();
                 }
                 else
                 {
@@ -49,18 +52,38 @@ namespace VRF
                 {
                     _HoldingObject.velocity = (transform.position - _HoldingObject.transform.position) / Time.fixedDeltaTime;
 
-                    if (!_HoldingObject.gameObject.tag.Equals("Handle"))
-                    {
-                        _HoldingObject.maxAngularVelocity = 20;
-                        Quaternion RotationDelta = transform.rotation * Quaternion.Inverse(_HoldingObject.transform.rotation);
-                        Vector3 RotationEuler = new Vector3(Mathf.DeltaAngle(0, RotationDelta.eulerAngles.x), Mathf.DeltaAngle(0, RotationDelta.eulerAngles.y), Mathf.DeltaAngle(0, RotationDelta.eulerAngles.z));
-                        RotationEuler *= 0.95f;
-                        RotationEuler *= Mathf.Deg2Rad;
-                        _HoldingObject.angularVelocity = RotationEuler / Time.fixedDeltaTime;
-                    }
+                    _HoldingObject.maxAngularVelocity = 20;
+                    Quaternion RotationDelta = transform.rotation * Quaternion.Inverse(_HoldingObject.transform.rotation);
+                    Vector3 RotationEuler = new Vector3(Mathf.DeltaAngle(0, RotationDelta.eulerAngles.x), Mathf.DeltaAngle(0, RotationDelta.eulerAngles.y), Mathf.DeltaAngle(0, RotationDelta.eulerAngles.z));
+                    RotationEuler *= 0.95f;
+                    RotationEuler *= Mathf.Deg2Rad;
+                    _HoldingObject.angularVelocity = RotationEuler / Time.fixedDeltaTime;
                 }
             }
             #endregion
+
+            #region Interacting_Object
+            if (!IsHandInteracting)
+            {
+                Collider[] Interactables = Physics.OverlapSphere(transform.position, PICKUP_DISTANCE, _InteractLayer);
+                if (Interactables.Length > 0)
+                {
+                    _InteractingObject = Interactables[0].GetComponent<Rigidbody>();
+                }
+                else
+                {
+                    _InteractingObject = null;
+                }
+            }
+            else
+            {
+                if (_InteractingObject != null)
+                {
+                    _InteractingObject.velocity =  2 * (transform.position - _InteractingObject.transform.position) / Time.fixedDeltaTime;
+                }
+            }
+            #endregion
+
             if (SteamVR_Actions.VRF.D_Down.GetStateDown(CurrentHandInput))
             {
                 EntityDriver.Instance.TriggerReelHold();
@@ -68,7 +91,7 @@ namespace VRF
 
             if (SteamVR_Actions.VRF.D_Down.GetStateUp(CurrentHandInput))
             {
-                //EntityDriver.Instance.TriggerReelHoldRelease();
+                EntityDriver.Instance.TriggerReelHoldRelease();
             }
         }
     }
