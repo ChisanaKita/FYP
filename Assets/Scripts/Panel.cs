2c5b51c253c83789a37420a4d4a17b74516c64d4
leochau15 <leochau46@gmail.com>
2021-04-29 22:28:25 +0800

    Add new fish and Panel script
    this panel script cannot add to the canvas and panel. But it works in another project that i created for test canvas

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace VRF
+{
+    public class Panel : MonoBehaviour
+    {
+        private Canvas canvas = null;
+        private PanelManager panelManager = null;
+        public Text product_Num;
+        public int currentNum = 0;
+
+        private void Awake()
+        {
+            canvas = GetComponent<Canvas>();
+        }
+
+        // Update is called once per frame
+        public void Setup()
+        {
+            this.panelManager = panelManager;
+            Hide();
+            product_Num.text = "0";
+        }
+
+        public void Show()
+        {
+            canvas.enabled = true;
+
+        }
+
+        public void Hide()
+        {
+            canvas.enabled = false;
+            currentNum = 0;
+            product_Num.text = currentNum.ToString();
+        }
+
+        public void Plus()
+        {
+            currentNum++;
+            product_Num.text = currentNum.ToString();
+
+        }
+        public void Minus()
+        {
+            if (currentNum > 0)
+                currentNum--;
+            product_Num.text = currentNum.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/PanelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace VRF
+{
+    public class PanelManager : MonoBehaviour
+    {
+        public Panel Panel1 = null;
+        public Panel Panel2 = null;
+        public Panel Panel3 = null;
+
+        private void Start()
+        {
+            Panel1.Show();
+        }
+
+        public void OnClickBackpackButton(Panel panel1)
+        {
+            Panel2.Hide();
+            Panel3.Hide();
+            Panel1.Show();
+        }
+        public void OnClickToolButton(Panel panel2)
+        {
+            Panel1.Hide();
+            Panel3.Hide();
+            Panel2.Show();
+        }
+        public void OnClickResourceButton(Panel panel3)
+        {
+            Panel1.Hide();
+            Panel2.Hide();
+            Panel3.Show();
+        }
+    }
+}
