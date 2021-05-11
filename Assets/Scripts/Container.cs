ea7d43674ae2b4fdc68b97450d21ac1e66f6e19d
ChisanaKita <29787181+ChisanaKita@users.noreply.github.com>
2021-05-11 13:10:53 +0800

    Fixed Red Fish Missing Colliders and Container class
    - Added Colliders to Red Fish's prefab
    - Added Container script
    
    - Changed Level1 and level2 fishes' tag to `Fish`

---
+++ Assets/Scripts/Container.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRF
+{
+    public class Container : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Fish")
+            {
+                Player.Instance.AddItem(other.gameObject);
+            }
+        }
+    }
+}
+
