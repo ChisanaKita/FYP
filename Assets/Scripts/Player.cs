064ad059a778525edd9b42dd9eb3e80afeea8a88
ChisanaKita <29787181+ChisanaKita@users.noreply.github.com>
2021-05-11 12:29:17 +0800

    Initial Player Data Implementation
    - Added singleten Player class and PlayerObj class for save-data
    `The Player class handles all I/O of game data, PlayerObj class is used as a wapper class for JSON.`
    
    - Changed `TestBait` to `FirstBait`
    - Changed Market scene panel's initial state to `Main_Panel`

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using VRF.BaseClass;
+
+namespace VRF
+{
+    public class Player : Singleten<Player>
+    {
+        private Player() { }
+
+        private PlayerObj PlayerObj;
+
+        private string SavePath = Application.persistentDataPath + "SaveData.json";
+
+        public void GetPlayerObj()
+        {
+            //
+            string data = GetJSONstring();
+            PlayerObj = data == "FirstTime" ? new PlayerObj() : JsonUtility.FromJson<PlayerObj>(data);
+        }
+
+        private string GetJSONstring()
+        {
+            FileStream file;
+
+            if (File.Exists(SavePath))
+            {
+                file = File.OpenRead(SavePath);
+            }
+            else
+            {
+                return "FirstTime";
+            }
+
+            BinaryReader binaryReader = new BinaryReader(file);
+            string RawData = binaryReader.ReadString();
+            binaryReader.Close();
+            file.Close();
+            return RawData;
+        }
+
+        public void SavePlayerObj()
+        {
+            //
+            FileStream file = File.Exists(SavePath) ? File.OpenWrite(SavePath) : File.Create(SavePath);
+            BinaryWriter binaryWriter = new BinaryWriter(file);
+            binaryWriter.Write(JsonUtility.ToJson(PlayerObj));
+            binaryWriter.Close();
+            file.Close();
+        }
+
+        public void AddItem(GameObject gameObject)
+        {
+            //
+            PlayerObj.Inventory.Add(gameObject);
+        }
+
+        public void RemoveItem(GameObject gameObject)
+        {
+            //
+            PlayerObj.Inventory.Remove(gameObject);
+        }
+
+        public void RemoveAll()
+        {
+            PlayerObj.Inventory.RemoveRange(0, PlayerObj.Inventory.Count);
+        }
+
+        public void SetCurrent_S_Bait(GameObject gameObject)
+        {
+            //
+            PlayerObj.Current_S_Bait = gameObject;
+        }
+
+        public void SetCurrent_M_Bait(GameObject gameObject)
+        {
+            //
+            PlayerObj.Current_M_Bait = gameObject;
+        }
+
+        public void SetCurrent_L_Bait(GameObject gameObject)
+        {
+            //
+            PlayerObj.Current_L_Bait = gameObject;
+        }
+
+        public GameObject Get_S_Bait()
+        {
+            return PlayerObj.Current_S_Bait;
+        } 
+
+        public GameObject Get_M_Bait()
+        {
+            return PlayerObj.Current_M_Bait;
+        }
+        public GameObject Get_L_Bait()
+        {
+            return PlayerObj.Current_L_Bait;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Utilities/PlayerObj.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRF
+{
+    [System.Serializable]
+    public class PlayerObj
+    {
+        public List<GameObject> Inventory;
+        public GameObject Current_S_Bait;
+        public GameObject Current_M_Bait;
+        public GameObject Current_L_Bait;
+        public int Balance;
+
+        public PlayerObj()
+        {
+            Inventory = new List<GameObject>();
+            Current_S_Bait = Resources.Load<GameObject>("Prefab/Baits/FirstBait");
+            Current_M_Bait = null;
+            Current_L_Bait = null;
+            Balance = 1000;
+        }
+    }
+}
+
