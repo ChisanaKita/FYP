3a52f6c149d24c48ea73121a1b0e4ce98979789d
Leong15 <leochau46@gmail.com>
2021-05-12 23:22:35 +0800

    update Market Scene
    -Import new Wooden assets for canvas UI
    
    -make UI more good looking
    -add code in SystemActions
     -switch view button
     -plus and minus button
     - disable sell all button when user
        isn't on inventory view

--- Assets/Scripts/Utilities/SystemActions.cs
+++ Assets/Scripts/Utilities/SystemActions.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace VRF.Util
 {
     public class SystemActions : MonoBehaviour
     {
+        public GameObject Inventory_view;
+        public GameObject Resource_view;
+        public GameObject Tool_view;
+
+        public Text product_Num;
+        public int currentNum = 0;
+        public Button SellAllBTN;
+
+        private void Start()
+        {
+            Inventory_view.SetActive(true);
+            Resource_view.SetActive(false);
+            Tool_view.SetActive(false);
+            product_Num.text = "0";
+        }
+
         public void Quit()
         {
             Player.Instance.SavePlayerObjectAndQuit();
@@ -35,6 +52,41 @@ namespace VRF.Util
                     break;
                 }
             }
+        }
+
+        public void OnClickInventoryButton(GameObject Inventory_view)
+        {
+
+            Inventory_view.SetActive(true);
+            Resource_view.SetActive(false);
+            Tool_view.SetActive(false);
+            SellAllBTN.enabled = true;
+        }
+        public void OnClickToolButton(GameObject Tool_view)
+        {
+            Tool_view.SetActive(true);
+            Resource_view.SetActive(false);
+            Inventory_view.SetActive(false);
+            SellAllBTN.enabled = false;
+        }
+        public void OnClickResourceButton(GameObject Resource_view)
+        {
+            Resource_view.SetActive(true);
+            Tool_view.SetActive(false);
+            Inventory_view.SetActive(false);
+            SellAllBTN.enabled = false;
+        }
+
+        public void Plus()
+        {
+            currentNum++;
+            product_Num.text = currentNum.ToString();
+        }
+        public void Minus()
+        {
+            if (currentNum > 0)
+                currentNum--;
+            product_Num.text = currentNum.ToString();
         }
     }
 }
